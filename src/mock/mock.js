6e1a132d397cbb06c1f839137038eb997aa3737c
Woogie-94 <keu1106@naver.com>
2021-12-16 18:05:07 +0900

    [add] db 연동 전 임시 mock 데이터

---
+++ src/mock/mock.js
@@ -0,0 +1,314 @@
+export const data = {
+  혼자: [
+    {
+      title_give: "출출한 밤 야식",
+      contents_give: [
+        { name: "치킨", percent: 25 },
+        { name: "닭발", percent: 25 },
+        { name: "족발", percent: 25 },
+        { name: "피자", percent: 25 },
+      ],
+    },
+    {
+      title_give: "분식 최고",
+      contents_give: [
+        { name: "김밥", percent: 30 },
+        { name: "쫄면", percent: 15 },
+        { name: "떡볶이", percent: 20 },
+        { name: "돈까스", percent: 25 },
+        { name: "라면", percent: 10 },
+      ],
+    },
+    {
+      title_give: "일본스러운 일식",
+      contents_give: [
+        { name: "초밥", percent: 25 },
+        { name: "돈까스", percent: 25 },
+        { name: "규동", percent: 25 },
+        { name: "소바", percent: 25 },
+      ],
+    },
+    {
+      title_give: "고기고기고기",
+      contents_give: [
+        { name: "삼겹살", percent: 25 },
+        { name: "등심", percent: 25 },
+        { name: "스테이크", percent: 25 },
+        { name: "돈까스", percent: 25 },
+      ],
+    },
+  ],
+  친구: [
+    {
+      title_give: "출출한 밤 야식",
+      contents_give: [
+        { name: "치킨", percent: 25 },
+        { name: "닭발", percent: 25 },
+        { name: "족발", percent: 25 },
+        { name: "피자", percent: 25 },
+      ],
+    },
+    {
+      title_give: "분식 최고",
+      contents_give: [
+        { name: "김밥", percent: 30 },
+        { name: "쫄면", percent: 15 },
+        { name: "떡볶이", percent: 20 },
+        { name: "돈까스", percent: 25 },
+        { name: "라면", percent: 10 },
+      ],
+    },
+    {
+      title_give: "일본스러운 일식",
+      contents_give: [
+        { name: "초밥", percent: 25 },
+        { name: "돈까스", percent: 25 },
+        { name: "규동", percent: 25 },
+        { name: "소바", percent: 25 },
+      ],
+    },
+    {
+      title_give: "고기고기고기",
+      contents_give: [
+        { name: "삼겹살", percent: 25 },
+        { name: "등심", percent: 25 },
+        { name: "스테이크", percent: 25 },
+        { name: "돈까스", percent: 25 },
+      ],
+    },
+  ],
+  회사: [
+    {
+      title_give: "출출한 밤 야식",
+      contents_give: [
+        { name: "치킨", percent: 25 },
+        { name: "닭발", percent: 25 },
+        { name: "족발", percent: 25 },
+        { name: "피자", percent: 25 },
+      ],
+    },
+    {
+      title_give: "분식 최고",
+      contents_give: [
+        { name: "김밥", percent: 30 },
+        { name: "쫄면", percent: 15 },
+        { name: "떡볶이", percent: 20 },
+        { name: "돈까스", percent: 25 },
+        { name: "라면", percent: 10 },
+      ],
+    },
+    {
+      title_give: "일본스러운 일식",
+      contents_give: [
+        { name: "초밥", percent: 25 },
+        { name: "돈까스", percent: 25 },
+        { name: "규동", percent: 25 },
+        { name: "소바", percent: 25 },
+      ],
+    },
+    {
+      title_give: "고기고기고기",
+      contents_give: [
+        { name: "삼겹살", percent: 25 },
+        { name: "등심", percent: 25 },
+        { name: "스테이크", percent: 25 },
+        { name: "돈까스", percent: 25 },
+      ],
+    },
+  ],
+  가족: [
+    {
+      title_give: "출출한 밤 야식",
+      contents_give: [
+        { name: "치킨", percent: 25 },
+        { name: "닭발", percent: 25 },
+        { name: "족발", percent: 25 },
+        { name: "피자", percent: 25 },
+      ],
+    },
+    {
+      title_give: "분식 최고",
+      contents_give: [
+        { name: "김밥", percent: 30 },
+        { name: "쫄면", percent: 15 },
+        { name: "떡볶이", percent: 20 },
+        { name: "돈까스", percent: 25 },
+        { name: "라면", percent: 10 },
+      ],
+    },
+    {
+      title_give: "일본스러운 일식",
+      contents_give: [
+        { name: "초밥", percent: 25 },
+        { name: "돈까스", percent: 25 },
+        { name: "규동", percent: 25 },
+        { name: "소바", percent: 25 },
+      ],
+    },
+    {
+      title_give: "고기고기고기",
+      contents_give: [
+        { name: "삼겹살", percent: 25 },
+        { name: "등심", percent: 25 },
+        { name: "스테이크", percent: 25 },
+        { name: "돈까스", percent: 25 },
+      ],
+    },
+  ],
+  회식: [
+    {
+      title_give: "출출한 밤 야식",
+      contents_give: [
+        { name: "치킨", percent: 25 },
+        { name: "닭발", percent: 25 },
+        { name: "족발", percent: 25 },
+        { name: "피자", percent: 25 },
+      ],
+    },
+    {
+      title_give: "분식 최고",
+      contents_give: [
+        { name: "김밥", percent: 30 },
+        { name: "쫄면", percent: 15 },
+        { name: "떡볶이", percent: 20 },
+        { name: "돈까스", percent: 25 },
+        { name: "라면", percent: 10 },
+      ],
+    },
+    {
+      title_give: "일본스러운 일식",
+      contents_give: [
+        { name: "초밥", percent: 25 },
+        { name: "돈까스", percent: 25 },
+        { name: "규동", percent: 25 },
+        { name: "소바", percent: 25 },
+      ],
+    },
+    {
+      title_give: "고기고기고기",
+      contents_give: [
+        { name: "삼겹살", percent: 25 },
+        { name: "등심", percent: 25 },
+        { name: "스테이크", percent: 25 },
+        { name: "돈까스", percent: 25 },
+      ],
+    },
+  ],
+  비오는날: [
+    {
+      title_give: "출출한 밤 야식",
+      contents_give: [
+        { name: "치킨", percent: 25 },
+        { name: "닭발", percent: 25 },
+        { name: "족발", percent: 25 },
+        { name: "피자", percent: 25 },
+      ],
+    },
+    {
+      title_give: "분식 최고",
+      contents_give: [
+        { name: "김밥", percent: 30 },
+        { name: "쫄면", percent: 15 },
+        { name: "떡볶이", percent: 20 },
+        { name: "돈까스", percent: 25 },
+        { name: "라면", percent: 10 },
+      ],
+    },
+    {
+      title_give: "일본스러운 일식",
+      contents_give: [
+        { name: "초밥", percent: 25 },
+        { name: "돈까스", percent: 25 },
+        { name: "규동", percent: 25 },
+        { name: "소바", percent: 25 },
+      ],
+    },
+    {
+      title_give: "고기고기고기",
+      contents_give: [
+        { name: "삼겹살", percent: 25 },
+        { name: "등심", percent: 25 },
+        { name: "스테이크", percent: 25 },
+        { name: "돈까스", percent: 25 },
+      ],
+    },
+  ],
+  소개팅: [
+    {
+      title_give: "출출한 밤 야식",
+      contents_give: [
+        { name: "치킨", percent: 25 },
+        { name: "닭발", percent: 25 },
+        { name: "족발", percent: 25 },
+        { name: "피자", percent: 25 },
+      ],
+    },
+    {
+      title_give: "분식 최고",
+      contents_give: [
+        { name: "김밥", percent: 30 },
+        { name: "쫄면", percent: 15 },
+        { name: "떡볶이", percent: 20 },
+        { name: "돈까스", percent: 25 },
+        { name: "라면", percent: 10 },
+      ],
+    },
+    {
+      title_give: "일본스러운 일식",
+      contents_give: [
+        { name: "초밥", percent: 25 },
+        { name: "돈까스", percent: 25 },
+        { name: "규동", percent: 25 },
+        { name: "소바", percent: 25 },
+      ],
+    },
+    {
+      title_give: "고기고기고기",
+      contents_give: [
+        { name: "삼겹살", percent: 25 },
+        { name: "등심", percent: 25 },
+        { name: "스테이크", percent: 25 },
+        { name: "돈까스", percent: 25 },
+      ],
+    },
+  ],
+  단체: [
+    {
+      title_give: "출출한 밤 야식",
+      contents_give: [
+        { name: "치킨", percent: 25 },
+        { name: "닭발", percent: 25 },
+        { name: "족발", percent: 25 },
+        { name: "피자", percent: 25 },
+      ],
+    },
+    {
+      title_give: "분식 최고",
+      contents_give: [
+        { name: "김밥", percent: 30 },
+        { name: "쫄면", percent: 15 },
+        { name: "떡볶이", percent: 20 },
+        { name: "돈까스", percent: 25 },
+        { name: "라면", percent: 10 },
+      ],
+    },
+    {
+      title_give: "일본스러운 일식",
+      contents_give: [
+        { name: "초밥", percent: 25 },
+        { name: "돈까스", percent: 25 },
+        { name: "규동", percent: 25 },
+        { name: "소바", percent: 25 },
+      ],
+    },
+    {
+      title_give: "고기고기고기",
+      contents_give: [
+        { name: "삼겹살", percent: 25 },
+        { name: "등심", percent: 25 },
+        { name: "스테이크", percent: 25 },
+        { name: "돈까스", percent: 25 },
+      ],
+    },
+  ],
+};
