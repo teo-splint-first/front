d96dc428ce7fa586ecb3caca00de00c7d40f0d5d
Jdentity <identityjang@gmail.com>
2021-12-16 20:17:53 +0900

    [feat] 룰렛 페이지 작업 푸쉬

--- src/components/pages/Roulette.js
+++ src/components/pages/Roulette.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router";
+import Template from "../common/Template";
+
 
 const Roulette = () => {
-  return <>Roulette</>;
+  const navigate = useNavigate();
+  const { state: roulette } = useLocation();
+  
+  return (
+    <Template goBackBtn>
+      
+    </Template>
+  );
 };
 
 export default Roulette;
