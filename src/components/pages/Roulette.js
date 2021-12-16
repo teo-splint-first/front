a10bb86b832b108c7e0f449b005e5496ea3c3f72
Jdentity <identityjang@gmail.com>
2021-12-16 20:37:30 +0900

    [feat] 룰렛 페이지
    중간 커밋

--- src/components/pages/Roulette.js
+++ src/components/pages/Roulette.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router";
+import styled from "styled-components";
 import Template from "../common/Template";
 
 const Roulette = () => {
@@ -9,16 +10,32 @@ const Roulette = () => {
   useEffect(() => {
     var ctx = canvasRef.current.getContext("2d");
     ctx.beginPath();
-    ctx.arc(50, 50, 20, 0, Math.PI * 2);
+    ctx.arc(100, 100, 20, 0, Math.PI * 2);
     ctx.fillStyle = "blue";
     ctx.fill();
     ctx.stroke();
   }, []);
+
   return (
     <Template goBackBtn>
       <canvas ref={canvasRef} width="150" height="150"></canvas>
+      <LinkButton onClick={() => navigate("/make")}>수정</LinkButton>
+      <LinkButton>돌리기</LinkButton>
     </Template>
   );
 };
+
+const LinkButton = styled.button`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  margin-bottom: 10px;
+  font-size: 25px;
+  color: #fff;
+  background-color: #10af8d;
+`;
 
 export default Roulette;
