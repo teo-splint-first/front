1666aaa1c7d0d32327108753f5e64c88365d0df6
maintainker <syhwan88@naver.com>
2021-12-16 20:23:06 +0900

    [feat] canvas

--- src/components/pages/Roulette.js
+++ src/components/pages/Roulette.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router";
 import Template from "../common/Template";
-
 
 const Roulette = () => {
   const navigate = useNavigate();
   const { state: roulette } = useLocation();
-  
+  const canvasRef = useRef();
+  useEffect(() => {
+    var ctx = canvasRef.current.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(50, 50, 20, 0, Math.PI * 2);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.stroke();
+  }, []);
   return (
     <Template goBackBtn>
-      
+      <canvas ref={canvasRef} width="150" height="150"></canvas>
     </Template>
   );
 };
