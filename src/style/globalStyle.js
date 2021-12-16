c23b030376cb26b588bb5e18598d96b6f4c5bfd6
Woogie-94 <keu1106@naver.com>
2021-12-16 16:05:48 +0900

    [fix] 디자인 시안에 맞게 자간 적용했습니다.

--- src/style/globalStyle.js
+++ src/style/globalStyle.js
@@ -12,6 +12,7 @@ export const GlobalStyle = createGlobalStyle`
   margin:0;
   padding:0;
   font-family: 'NanumSquareEB' !important;
+  letter-spacing: -2px;
   &:focus,&:hover,&:active{
   outline:none !important;
   }
